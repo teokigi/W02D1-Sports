bcd59ce39c8c35bc86b3aa3760d3ee4f531e8933
Tony Tang <teokigi@hotmail.com>
2020-03-16 18:55:25 +0000

    test fails*3

--- specs/sports_spec.rb
+++ specs/sports_spec.rb
@@ -21,15 +21,15 @@ class TestSports < MiniTest::Test
     end
 
     def test_001_get_team_name_and_return_glasgow_gladiators
-        assert_equal("glasgow gladiators",@team_gg.get_name)
+        assert_equal("glasgow gladiator",@team_gg.get_name)
     end
 
     def test_002_get_team_roster
-        assert_equal(["jimmy","jack","jake","jeremy","joey","joseph","jerry"], @team_gg.get_roster)
+        assert_equal(["jimm","jack","jake","jeremy","joey","joseph","jerry"], @team_gg.get_roster)
     end
 
     def test_003_get_team_coach_return_williamson
-        assert_equal("williamson",@team_gg.get_coach)
+        assert_equal("williamso",@team_gg.get_coach)
     end
 
 end
