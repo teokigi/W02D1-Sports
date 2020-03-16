f52c9983243bb951681d5c6cd519f2eb35cd30bd
Tony Tang <teokigi@hotmail.com>
2020-03-16 18:10:14 +0000

    added 1 getter method per class parameter

--- sports.rb
+++ sports.rb
@@ -1,9 +1,22 @@
 class sports
 
-attr_accessor :team_name, :players_on_team, :team_coach,
+# attr_accessor :team_name, :players_on_team, :team_coach,
 
   def initialize(name,roster,coach,points)
       @name = name,
       @roster = roster,
-      @coach = coach,
+      @coach = coach
+  end
+
+  def get_name
+      return @name
+  end
+
+  def get_roster
+      return @roster
+  end
+
+  def get_coach
+      return @coach
+  end
 end
