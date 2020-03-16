6d29b1b1ff5eae18cd88f79dcd923d7468987975
Tony Tang <teokigi@hotmail.com>
2020-03-16 19:01:39 +0000

    added instructional comments

--- specs/sports_spec.rb
+++ specs/sports_spec.rb
@@ -4,7 +4,7 @@ require('minitest/reporters')
 require_relative('../sports.rb')
 
 class TestSports < MiniTest::Test
-
+    #setup initial values
     def setup
 
         roster = [
@@ -19,17 +19,22 @@ class TestSports < MiniTest::Test
         @team_gg = SportsTeam.new("glasgow gladiators", roster, "williamson")
 
     end
-
+    #test get name function
     def test_001_get_team_name_and_return_glasgow_gladiators
-        assert_equal("glasgow gladiator",@team_gg.get_name)
+        assert_equal("glasgow gladiators",@team_gg.get_name)
     end
-
+    #test get team roster function
     def test_002_get_team_roster
-        assert_equal(["jimm","jack","jake","jeremy","joey","joseph","jerry"], @team_gg.get_roster)
+        assert_equal(["jimmy","jack","jake","jeremy","joey","joseph","jerry"], @team_gg.get_roster)
     end
-
+    #test get team coach function
     def test_003_get_team_coach_return_williamson
-        assert_equal("williamso",@team_gg.get_coach)
+        assert_equal("williamson",@team_gg.get_coach)
     end
+    #test set team name function
+    
+    #test adding to roster
+    #test removing from roster
+    #test setting new coach
 
 end
--- sports.rb
+++ sports.rb
@@ -7,16 +7,21 @@ class SportsTeam
       @roster = roster
       @coach = coach
   end
-
+  #get team name
   def get_name
       return @name
   end
-
+  #get team roster list
   def get_roster
       return @roster
   end
-
+  #get team coach
   def get_coach
       return @coach
   end
+  #set team name
+  #add new team member to roster
+  #remove team member from roster
+  #set team coach
+
 end
