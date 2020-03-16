089d24d6e02f9e4c0ba94adb697be738d2a45ce6
Tony Tang <teokigi@hotmail.com>
2020-03-16 18:54:49 +0000

    test pass

--- specs/sports_spec.rb
+++ specs/sports_spec.rb
@@ -6,28 +6,30 @@ require_relative('../sports.rb')
 class TestSports < MiniTest::Test
 
     def setup
-        team_gg = SportsTeam.new("glasgow_gladiators",
-                                                [
-                                                    "jimmy",
-                                                    "jack",
-                                                    "jake",
-                                                    "jeremy",
-                                                    "joey",
-                                                    "joseph",
-                                                    "jerry"],
-                                                "williamson")
+
+        roster = [
+                "jimmy",
+                "jack",
+                "jake",
+                "jeremy",
+                "joey",
+                "joseph",
+                "jerry"]
+
+        @team_gg = SportsTeam.new("glasgow gladiators", roster, "williamson")
+
     end
 
     def test_001_get_team_name_and_return_glasgow_gladiators
-        assert_equal("glasgow_gladiators",team_gg.get_name)
+        assert_equal("glasgow gladiators",@team_gg.get_name)
     end
 
     def test_002_get_team_roster
-        assert_equal(["jimmy","jack","jake","jeremy","joey","joseph","jerry"], team_gg.get_roster)
+        assert_equal(["jimmy","jack","jake","jeremy","joey","joseph","jerry"], @team_gg.get_roster)
     end
 
     def test_003_get_team_coach_return_williamson
-        assert_equal("williamson",team_gg.get_coach)
+        assert_equal("williamson",@team_gg.get_coach)
     end
-    
+
 end
--- sports.rb
+++ sports.rb
@@ -2,9 +2,9 @@ class SportsTeam
 
 # attr_accessor :team_name, :players_on_team, :team_coach,
 
-  def initialize(name,roster,coach,points)
-      @name = name,
-      @roster = roster,
+  def initialize(name,roster,coach)
+      @name = name
+      @roster = roster
       @coach = coach
   end
 
