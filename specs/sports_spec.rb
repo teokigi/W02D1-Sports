fcc1c2685b2c6f23d54ff777fc7173fc0c2bfe37
Tony Tang <teokigi@hotmail.com>
2020-03-16 20:32:07 +0000

    converted getter/setter to use attr_accessor

--- specs/sports_spec.rb
+++ specs/sports_spec.rb
@@ -21,37 +21,37 @@ class TestSports < MiniTest::Test
     end
     #test get name function
     def test_001_get_team_name_and_return_glasgow_gladiators
-        assert_equal("glasgow gladiators",@team_gg.get_name)
+        assert_equal("glasgow gladiators",@team_gg.name)
     end
     #test get team roster function
     def test_002_get_team_roster
-        assert_equal(["jimmy","jack","jake","jeremy","joey","joseph","jerry"], @team_gg.get_roster)
+        assert_equal(["jimmy","jack","jake","jeremy","joey","joseph","jerry"], @team_gg.roster)
     end
     #test get team coach function
     def test_003_get_team_coach_return_williamson
-        assert_equal("williamson",@team_gg.get_coach)
+        assert_equal("williamson",@team_gg.coach)
     end
     #test set team name function
     def test_004_set_team_name_glasgow_gluttons
-        @team_gg.set_name("glasgow gluttons")
-        assert_equal("glasgow gluttons", @team_gg.get_name)
+        @team_gg.name = "glasgow gluttons"
+        assert_equal("glasgow gluttons", @team_gg.name)
     end
     #test adding to roster
     def test_005_add_to_roster_jeff
-        @team_gg.add_to_roster("jeff")
-        assert_equal(8,@team_gg.get_roster.length)
+        @team_gg.roster.push("jeff")
+        assert_equal(8,@team_gg.roster.length)
     end
 
     #test removing from roster
     def test_006_remove_from_roster_by_name
-        @team_gg.remove_roster("jimmy")
-        assert_equal(6,@team_gg.get_roster.length)
+        @team_gg.roster.delete("jimmy")
+        assert_equal(6,@team_gg.roster.length)
     end
 
     #test setting new coach
     def test_007_set_coach_hendrix
-        @team_gg.set_coach("hendrix")
-        assert_equal("hendrix",@team_gg.get_coach)
+        @team_gg.coach = "hendrix"
+        assert_equal("hendrix",@team_gg.coach)
     end
 
 end
--- sports.rb
+++ sports.rb
@@ -1,39 +1,45 @@
 class SportsTeam
-
-# attr_accessor :team_name, :players_on_team, :team_coach,
+    #allows access to team attributes
+    attr_accessor :name, :roster, :coach,
 
   def initialize(name,roster,coach)
       @name = name
       @roster = roster
       @coach = coach
   end
+  #old getter functions
+  #---------------------
   #get team name
-  def get_name
-      return @name
-  end
-  #get team roster list
-  def get_roster
-      return @roster
-  end
-  #get team coach
-  def get_coach
-      return @coach
-  end
+  # def get_name
+  #     return @name
+  # end
+  # #get team roster list
+  # def get_roster
+  #     return @roster
+  # end
+  # #get team coach
+  # def get_coach
+  #     return @coach
+  # end
+  #----------------------
+  #old setter functions
+  #----------------------
   #set team name
-  def set_name(new_name)
-      @name = new_name
-  end
+  # def set_name(new_name)
+  #     @name = new_name
+  # end
   #add new team member to roster
-  def add_to_roster(new_player)
-      @roster.push(new_player)
-  end
-  #remove team member from roster
-  def remove_roster(player)
-      @roster.delete(player)
-  end
-  #set team coach
-  def set_coach(new_coach)
-      @coach = new_coach
-  end
+  # def add_to_roster(new_player)
+  #     @roster.push(new_player)
+  # end
+  # # #remove team member from roster
+  # def remove_roster(player)
+  #     @roster.delete(player)
+  # end
+  # #set team coach
+  # def set_coach(new_coach)
+  #     @coach = new_coach
+  # end
+  #--------------------
 #class end
 end
