9fcc459eef99158ee32055db3731f3772d0df188
Tony Tang <teokigi@hotmail.com>
2020-03-16 18:06:39 +0000

    opening commit

---
+++ specs/sports_spec.rb
@@ -0,0 +1,14 @@
+require('minitest/autorun')
+require('minitest/reporters')
+
+require_relative('../sports.rb')
+
+class TestSports < MiniTest::Test
+
+    def setup
+    end
+
+    def test_001
+    end
+
+end
---
+++ sports.rb
@@ -0,0 +1,9 @@
+class sports
+
+attr_accessor :team_name, :players_on_team, :team_coach,
+
+  def initialize(name,roster,coach,points)
+      @name = name,
+      @roster = roster,
+      @coach = coach,
+end
